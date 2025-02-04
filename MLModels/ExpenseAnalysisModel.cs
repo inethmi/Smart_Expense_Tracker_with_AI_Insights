37177de9c710e37b08a98ed27cc6bde69ffaf9c3
inethmi <it22367976@my.sliit.lk>
2025-02-04 21:36:00 +0530

    Your commit message here

---
+++ MLModels/ExpenseAnalysisModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML.Data;
+
+namespace SmartExpenseTracker.API.MLModels
+{
+    public class ExpenseAnalysisModel
+    {
+        [LoadColumn(0)]
+        public float Food { get; set; }
+
+        [LoadColumn(1)]
+        public float Transport { get; set; }
+
+        [LoadColumn(2)]
+        public float Entertainment { get; set; }
+
+        [LoadColumn(3)]
+        public float TotalSpending { get; set; }
+
+        [LoadColumn(4)]
+        public string Insight { get; set; } // AI-generated financial insight
+    }
+}
+
--- obj/Debug/net8.0/SmartExpenseTracker.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartExpenseTracker.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec8d97bfa35adabfda6a32ea325bda1317716225")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
