6fabaceeb7123e3eda040214b42d806ae21f3f2b
Inuri  Ediriweera <inu048ediriweera@gmail.com>
2025-02-16 23:36:38 +0530

    Add frontendExpenseTracker folder with frontend files

---
+++ frontendExpenseTracker/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
